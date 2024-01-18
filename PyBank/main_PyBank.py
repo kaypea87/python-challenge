8a62d9106f931edf233bba7a95119511b4ba53b0
kaypea87 <Komie@MacBook-Air-2.fios-router.home>
2024-01-17 22:59:58 -0500

    final

---
+++ PyBank/main_PyBank.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#code for PyBank
+
+#import datafile
+import pandas as pd
+from pathlib import Path
+#path to csv file and read
+budget_data = Path("Resources/budget_data.csv")
+budget_df = pd.read_csv(budget_data)
+budget_df.head()
+
+#The total number of months included in the dataset
+monthcount=len(budget_df)
+print("Financial Analysis")
+print("-------------------")
+print(f"Total Months : {monthcount}")
+#The net total amount of "Profit/Losses" over the entire period
+totalprofit = 0
+for i in range(monthcount):
+    #print(i)
+    #print(budget_df["Profit/Losses"][i])
+    totalprofit = totalprofit + budget_df["Profit/Losses"][i]
+
+
+print(f"Total Profit:$ {totalprofit}")
+    
+    
+#The changes in "Profit/Losses" over the entire period, and then the average of those changes
+#start average at 0
+averagesum = 0
+monthlychange=[]
+for i in range(1,monthcount):
+    monthlydiff = budget_df["Profit/Losses"][i]-budget_df["Profit/Losses"][i-1]
+    monthlychange.append(monthlydiff)
+    averagesum = averagesum + monthlydiff
+    
+averagechange = averagesum/(monthcount-1)
+print(f"Average Change: ${averagechange:.2f}")
+
+
+#The greatest increase in profits (date and amount) over the entire period
+maxvalue = monthlychange[0]
+for i in range(1,monthcount-1):
+    if maxvalue < monthlychange[i]:
+        maxvalue = monthlychange[i]
+        maxvaluedate = budget_df["Date"][i+1]
+        
+print(f"Greatest Increase in Profits: {maxvaluedate} (${maxvalue})")
+
+
+#The greatest decrease in profits (date and amount) over the entire period
+minvalue= monthlychange[0]
+for i in range(1,monthcount-1):
+    if minvalue > monthlychange[i]:
+        minvalue = monthlychange[i]
+        minvaluedate = budget_df["Date"][i+1]
+        
+print(f"Greatest Decrease in Profits: {minvaluedate} (${minvalue})")
+
+#write file to text
+file = open('analysis/output.txt','w',encoding ='UTF-8')
+file.write("Financial Analysis")
+file.write("---------------------")
+file.write(f"Total Months : {monthcount}")
+file.write(f"Total Profit:$ {totalprofit}")
+file.write(f"Average Change: ${averagechange:.2f}")
+file.write(f"Greatest Increase in Profits: {maxvaluedate} (${maxvalue})")
+file.write(f"Greatest Decrease in Profits: {minvaluedate} (${minvalue})")
+file.close()
+
---
+++ PyPoll/main_PyPoll.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 00:16:37 2024
+
+@author: Komie
+"""
+
+import pandas as pd
+from pathlib import Path
+
+#path to csv file and read
+election_data = Path("Resources/election_data.csv")
+election_df = pd.read_csv(election_data)
+election_df.head()
+
+#The total number of votes cast
+totalvotes = len(election_df)
+#print(f"Total Votes: {totalvotes}")
+#print(totalvotes)
+
+#A complete list of candidates who received votes, unique values
+candidate_list = election_df['Candidate'].unique()
+#print(candidate_list)
+
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+
+#create candidate votes dictionary
+candidate_votes = {}
+
+for candidate in candidate_list:
+    candidate_votes[candidate]=0
+#print("this is our starting dictionary")
+#print(candidate_votes)
+#print("")
+
+#print("let's start counting votes!")
+for i in range(totalvotes):
+    name = election_df['Candidate'][i]
+    #print(name)
+    candidate_votes[name] = candidate_votes[name] +1
+    #print(candidate_votes)
+#print(candidate_votes)
+
+#print("")
+#print("let's loop through the final dictionary!")
+
+#create votepercent dictionary to track the percentages
+votepercent={}
+for candidate in candidate_votes: 
+    votepercent[candidate] = candidate_votes[candidate] / totalvotes
+    
+    #print(candidate)
+    #print(candidate_votes[candidate])
+   # print(f"{votepercent:.3%}")
+
+#The winner of the election based on popular vote using max value in dictionary
+#source: https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+maxvotes = max(candidate_votes, key=candidate_votes.get)
+#print(maxvotes)
+
+print("Elections Results")
+print("-----------------------")
+print(f"Total Votes: {totalvotes}")
+print("-----------------------")
+for candidate in candidate_list:
+    print(f"{candidate} : {votepercent[candidate]:.3%} ({candidate_votes[candidate]})")
+print("-----------------------")
+print(f"Winner: {maxvotes}")
+print("-----------------------")
+
+#write fiile to text
+file = open('analysis/output.txt','w',encoding ='UTF-8')
+file.write("Election Results")
+file.write("-----------------------")
+file.write(f"Total Votes: {totalvotes}")
+file.write("-----------------------")
+for candidate in candidate_list:
+    file.write(f"{candidate} : {votepercent[candidate]:.3%} ({candidate_votes[candidate]})")
+file.write("-----------------------")
+file.write(f"Winner: {maxvotes}")
+file.write("-----------------------")
+file.close()
+
+
+
+
+
+
+
+
+
+
